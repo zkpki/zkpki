bff8261019c0956c8bb16d12b3b0799d23b274f7
Matt Peterson <matt@implbits.com>
2018-11-20 13:28:41 -0700

    um... I'm .gitignore'd the test directory. Not sure why.

---
+++ ZkpkiStorage/test/crypto/crypto.js
@@ -0,0 +1,31 @@
+﻿const assert = require('assert');
+const crypto = require('../../lib/crypto')
+
+describe("Storage tests for file provider", function () {
+    it("randomBytes()", () => {
+        var bytes = crypto.randomBytes(32);
+        assert.equal(bytes.length, 32);
+    });
+
+    it("hashBytes()", function () {
+        var clearText = "This is the clear text.  Can you see it?";
+        var hashValue = crypto.hashBytes(clearText);
+        
+        assert.equal(hashValue, "CcjornhoRXZ45nTn2FTUFMXf2huPZCNfXpHgbZ9CS31E");
+    });
+
+    it("encryptBytes() and decrytBytes()", function () {
+        var clearText = "This is the clear text.  Can you see it?";
+        var key = crypto.randomBytes(32);
+        var cypherText = crypto.encryptBytes(key, clearText);
+
+        assert.equal(crypto.decryptBytes(key, cypherText), clearText);
+    });
+
+    it("encryptBytes() and decryptBytes with empty buffer", () => {
+        var clearText = "";
+        var key = crypto.randomBytes(32);
+        var cypherText = crypto.encryptBytes(key, clearText);
+        assert.equal(crypto.decryptBytes(key, cypherText), clearText);
+    });
+});
---
+++ ZkpkiStorage/test/storage.js
@@ -0,0 +1,11 @@
+﻿var assert = require("assert");
+var storage = require("../lib/storage");
+
+describe("Storage", function () {
+
+    it("Check providers", async () => {        
+        assert.ok(storage.file, "Couldn't find storage.file");
+        assert.ok(storage.file.open, "Couldn't find storage.file doesn't have open()");       
+    });
+    
+});
