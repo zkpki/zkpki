6f29c2678faba3a7d86767e4a078a5e18cd565f0
petrsnd <petrsnd@gmail.com>
2018-12-23 00:43:37 -0700

    Forgot the base module unit tests

---
+++ ZkpkiModel/test/zkpki-model.js
@@ -0,0 +1,10 @@
+﻿const assert = require("assert");
+const zkpkiModel = require("../index.js");
+
+describe("ZKPKI Model",
+    function() {
+        it("Initialize",
+            function() {
+                assert.ok(false);
+            });
+    });
